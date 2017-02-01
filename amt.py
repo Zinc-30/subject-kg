0e3534600f6ddf09909924e3676b669e2fdfc926
xinhao <xinhao20122013@hotmail.com>
2017-02-01 14:58:36 +0800

    change amt to class

--- amt.py
+++ amt.py
@@ -1,244 +1,238 @@
 # imports the Boto library so that it can be used
 import boto
 import collections
+import pandas as pd
 from boto.mturk.layoutparam import LayoutParameter
 from boto.mturk.layoutparam import LayoutParameters
 # Tells Python where to find the MTurkConnection client in the boto library
 from boto.mturk.connection import MTurkConnection
 from boto.mturk.question import HTMLQuestion
+import pickle
 # Create your connection to MTurk
 
 ###############################################
 #Test Functions
-def _GET_Balance( mtc):
-	account_balance = mtc.get_account_balance()[0]
-	print ("You have a balance of: {}".format(account_balance))
-	return account_balance
-
-# This is the example of the tutorial that create HIT from HTML input
-def _Create_HIT (mtc):
-	question_html_value = """
-	<html>
-	<head>
-	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>
-	<script src='https://s3.amazonaws.com/mturk-public/externalHIT_v1.js' type='text/javascript'></script>
-	</head>
-	<body>
-	<!-- HTML to handle creating the HIT form -->
-	<form name='mturk_form' method='post' id='mturk_form' action='https://workersandbox.mturk.com/mturk/externalSubmit'>
-	<input type='hidden' value='' name='assignmentId' id='assignmentId'/>
-	<!-- This is where you define your question(s) --> 
-	<h1>Please name the company that created the Galaxy</h1>
-	<p><textarea name='answer' rows=3 cols=80></textarea></p>
+class Amt():
+	ACCESS_ID = 'AKIAJUT7UAPF2JZYLK6A'
+	SECRET_KEY = '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'
+	sandbox_HOST = 'mechanicalturk.sandbox.amazonaws.com'
+	real_HOST = 'mechanicalturk.amazonaws.com'
+	real_ID = ['33KTOXRB2LW4PI5DBFZPH8KMJK5FRN','34FHTOY99EQJL9R5QQSQRPJZ03RASC']
+	sandbox_ID = ['3MVRTDU8OEPFKV7FE8HQFIS83UU5FC','38X1F3X9FX9JBPRXU0O22B4V1CUY5X']
+	mtc = MTurkConnection(aws_access_key_id = ACCESS_ID, aws_secret_access_key = SECRET_KEY, host = sandbox_HOST)
 	
-	<!-- HTML to handle submitting the HIT -->
-	<p><input type='submit' id='submitButton' value='Submit' /></p></form>
-	<script language='Javascript'>turkSetAssignmentID();</script>
-	</body>
-	</html>
-	"""
-	# The first parameter is the HTML content
-	# The second is the height of the frame it will be shown in
-	# Check out the documentation on HTMLQuestion for more details
-	html_question = HTMLQuestion(question_html_value, 500)
-	# These parameters define the HIT that will be created
-	# question is what we defined above
-	# max_assignments is the # of unique Workers you're requesting
-	# title, description, and keywords help Workers find your HIT
-	# duration is the # of seconds Workers have to complete your HIT
-	# reward is what Workers will be paid when you approve their work
-	# Check out the documentation on CreateHIT for more details
-	response = mtc.create_hit(question=html_question,
-                          max_assignments=1,
-                          title="Answer a simple question",
-                          description="Help research a topic",
-                          keywords="question, answer, research",
-                          duration=120,
-                          reward=0.50)
-	# The response included several fields that will be helpful later
-	hit_type_id = response[0].HITTypeId
-	hit_id = response[0].HITId
-
-	myfile =  open ("HIT_Info1.txt","w")
-	myfile.write("{0}\n{1}\n{2}\n".format("Your HIT has been created. You can see it at this link:","https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id),
-	"Your HIT ID is: {}".format(hit_id)))
-	myfile.close();
-	print ("Your HIT has been created. You can see it at this link:")
-	print ("https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id))
-	print ("Your HIT ID is: {}".format(hit_id))
-	return{'hit_type_id':hit_type_id,'hit_id':hit_id}
-
-################################################################################
-# Create a single Hits of type City
-# Note that we fix the attributes of all the properties, so each type has its own HIT template
-# Parameters: {Fifth,First,Forth,Second,Third,attribute,type}
-# return{'hit_type_id':hit_type_id,'hit_id':hit_id}
-def _Create_City_Hit(mtc, ins_lis, contrylist ,attribtueName):
-
-	# These parameters define the HIT that will be created
-	# question is what we defined above
-	# max_assignments is the # of unique Workers you're requesting
-	# title, description, and keywords help Workers find your HIT
-	# duration is the # of seconds Workers have to complete your HIT
-	# reward is what Workers will be paid when you approve their work
-	# Check out the documentation on CreateHIT for more details
-	#instance_value = LayoutParameter('instance',instanceName)
-	attribute_value = LayoutParameter('attribute',attribtueName)
-	first_ins = LayoutParameter('First', ins_lis[0])
-	second_ins = LayoutParameter('Second', ins_lis[1])
-	third_ins = LayoutParameter('Third', ins_lis[2])
-	forth_ins = LayoutParameter('Forth', ins_lis[3])
-	fifth_ins = LayoutParameter('Fifth', ins_lis[4])
-	countryFirst = LayoutParameter('countryFirst', contrylist[0])
-	countrySecond = LayoutParameter('countrySecond', contrylist[1])
-	countryThird = LayoutParameter('countryThird', contrylist[2])
-	countryForth = LayoutParameter('countryForth', contrylist[3])
-	countryFifth = LayoutParameter('countryFifth', contrylist[4])
 	
-	type_value = LayoutParameter('type', 'City')
-	params = LayoutParameters([fifth_ins,first_ins,forth_ins,second_ins,third_ins,attribute_value,type_value,countryFirst,countrySecond,countryThird,countryForth,countryFifth])
-	response = mtc.create_hit(hit_type='3Z53LF8QZE90JGGUTI4YTOYIVBFFCT',
-							  hit_layout='3W3S1A7N3YDL5VIBRWHYJ4DPH2JZBP',
-							  layout_params = params
-							  )
-	# The response included several fields that will be helpful later
-	hit_type_id = response[0].HITTypeId
-	hit_id = response[0].HITId
+	def __init__(self, flag):
+		if flag:
+			self.hit_type = self.sandbox_ID[0]
+			self.hit_layout = self.sandbox_ID[1]
+		else:
+			self.hit_type = self.real_ID[0]
+			self.hit_layout = self.real_ID[1]
 
-	myfile =  open ("HIT_Info1.txt","a")
-	myfile.write("{0}\n{1}\n{2}\n".format("Your HIT has been created. You can see it at this link:","https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id),
-	"Your HIT ID is: {}".format(hit_id)))
-	myfile.close();
-	print ("Your HIT has been created. You can see it at this link:")
-	print ("https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id))
-	print ("Your HIT ID is: {}".format(hit_id))
-	return{'hit_id':hit_id}
+	def _GET_Balance(self):
+		mtc = self.mtc
+		account_balance = mtc.get_account_balance()[0]
+		print ("You have a balance of: {}".format(account_balance))
+		return account_balance
 
-# return Yes|No count dic
-# return attributeName dic
-#[yes_no_dic, attribute_dic]
-def _Retrive_HIT_Answer (mtc, hit_id):
-	# This is the value you reeceived when you created the HIT
-	# You can also retrieve HIT IDs by calling GetReviewableHITs
-	# and SearchHITs. See the links to read more about these APIs.
-	#result = mtc.get_assignments("3R5LWXWHR09AANE1160M46QDXFVXGA")
-	print("Retrive Answer for id:{0}".format(hit_id))
-
-	result = mtc.get_assignments(hit_id)
-	#print("result:{0}".format(result.NumResults))
-	# result is a list of assignments
-
-	# check the attributes of the assignments
-	# AcceptTime, Assignment, AssignmentId, AssignmentStatus, AutoApprovalTime, HITId, SubmitTime, WorkerId, answers
-	# result === xml <Assignment>
-
-	ins_result = []
-	attribute_result = []
-
-	for assignment in result:
-		assignmentid = assignment.AssignmentId
-		worker_id = assignment.WorkerId
-		# answers is a list of QuestionFormAnswer object
-		answerlist = assignment.answers
+	################################################################################
+	# Create a single Hits of type City
+	# Note that we fix the attributes of all the properties, so each type has its own HIT template
+	# Parameters: {Fifth,First,Forth,Second,Third,attribute,type}
+	# return{'hit_type_id':hit_type_id,'hit_id':hit_id}
+	def _Create_City_Hit(self, ins_lis, contrylist ,attribtueName):
+		mtc = self.mtc
+		# These parameters define the HIT that will be created
+		# question is what we defined above
+		# max_assignments is the # of unique Workers you're requesting
+		# title, description, and keywords help Workers find your HIT
+		# duration is the # of seconds Workers have to complete your HIT
+		# reward is what Workers will be paid when you approve their work
+		# Check out the documentation on CreateHIT for more details
+		#instance_value = LayoutParameter('instance',instanceName)
+		attribute_value = LayoutParameter('attribute',attribtueName)
+		first_ins = LayoutParameter('First', ins_lis[0])
+		second_ins = LayoutParameter('Second', ins_lis[1])
+		third_ins = LayoutParameter('Third', ins_lis[2])
+		forth_ins = LayoutParameter('Forth', ins_lis[3])
+		fifth_ins = LayoutParameter('Fifth', ins_lis[4])
+		countryFirst = LayoutParameter('countryFirst', contrylist[0])
+		countrySecond = LayoutParameter('countrySecond', contrylist[1])
+		countryThird = LayoutParameter('countryThird', contrylist[2])
+		countryForth = LayoutParameter('countryForth', contrylist[3])
+		countryFifth = LayoutParameter('countryFifth', contrylist[4])
 		
-		for answer in answerlist[0]:
-			# check the attributes of answer
-			#print("qid:{0}; value:{1}".format(answer.qid,answer.fields[0]))
+		type_value = LayoutParameter('type', 'City')
+		params = LayoutParameters([fifth_ins,first_ins,forth_ins,second_ins,third_ins,attribute_value,type_value,countryFirst,countrySecond,countryThird,countryForth,countryFifth])
+		response = mtc.create_hit(hit_type=self.hit_type,
+								  hit_layout=self.hit_layout,
+								  layout_params = params,
+								  max_assignments = 3,
+								  )
+		# The response included several fields that will be helpful later
+		hit_type_id = response[0].HITTypeId
+		hit_id = response[0].HITId
+
+		myfile =  open ("HIT_Info1.txt","a")
+		myfile.write("{0}\n{1}\n{2}\n".format("Your HIT has been created. You can see it at this link:","https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id),
+		"Your HIT ID is: {}".format(hit_id)))
+		myfile.close();
+		print ("Your HIT has been created. You can see it at this link:")
+		print ("https://workersandbox.mturk.com/mturk/preview?groupId={}".format(hit_type_id))
+		print ("Your HIT ID is: {}".format(hit_id))
+		return{'hit_id':hit_id}
+
+	# return Yes|No count dic
+	# return attributeName dic
+	#[yes_no_dic, attribute_dic]
+	def _Retrive_HIT_Answer (self, hit_id):
+		mtc = self.mtc
+		# This is the value you reeceived when you created the HIT
+		# You can also retrieve HIT IDs by calling GetReviewableHITs
+		# and SearchHITs. See the links to read more about these APIs.
+		#result = mtc.get_assignments("3R5LWXWHR09AANE1160M46QDXFVXGA")
+		print("Retrive Answer for id:{0}".format(hit_id))
+
+		result = mtc.get_assignments(hit_id)
+		#print("result:{0}".format(result.NumResults))
+		# result is a list of assignments
+
+		# check the attributes of the assignments
+		# AcceptTime, Assignment, AssignmentId, AssignmentStatus, AutoApprovalTime, HITId, SubmitTime, WorkerId, answers
+		# result === xml <Assignment>
+
+		ins_result = []
+		attribute_result = []
+
+		for assignment in result:
+			assignmentid = assignment.AssignmentId
+			worker_id = assignment.WorkerId
+			# answers is a list of QuestionFormAnswer object
+			answerlist = assignment.answers
 			
-			#if answer.qid == 'answer':
-			#	worker_answer = answer.fields[0]
-				# answer.field is a list, for this task, it has only one element
-				#return worker_answer
-
-			if answer.qid == 'Instance':
-				iList = answer.fields[0].split('|')
-				for ins in iList:
-					ins_result.append(ins)
+			for answer in answerlist[0]:
+				# check the attributes of answer
+				#print("qid:{0}; value:{1}".format(answer.qid,answer.fields[0]))
 				
+				#if answer.qid == 'answer':
+				#	worker_answer = answer.fields[0]
+					# answer.field is a list, for this task, it has only one element
+					#return worker_answer
 
-			if answer.qid == 'Attribute':
-				aList = answer.fields[0].split('|')
-				for attr in aList:
-					attribute_result.append(attr)
+				if answer.qid == 'Instance':
+					iList = answer.fields[0].split('|')
+					for ins in iList:
+						ins_result.append(ins)
+					
 
-       
-  			#if answer.qid == 'answer':
-  			#	worker_answer = answer.fields[0]
-  			#	print ("The Worker with ID {} submitted assignment {} \
-  			#		and gave the answer {}".format(worker_id, assignmentid, worker_answer))
-  				#return worker_answer
-	return dict(collections.Counter(ins_result)),dict(collections.Counter(attribute_result))
-	 
+				if answer.qid == 'Attribute':
+					aList = answer.fields[0].split('|')
+					for attr in aList:
+						attribute_result.append(attr)
 
-# retrive all reviewable HITs, return is a list of HITId
-def _Search_HIT(mtc):
-	hits = mtc.get_all_hits()
-	hits_id = []
-	for hit in hits :
-		hits_id.append(hit.HITId)
-	return hits_id
+	       
+	  			#if answer.qid == 'answer':
+	  			#	worker_answer = answer.fields[0]
+	  			#	print ("The Worker with ID {} submitted assignment {} \
+	  			#		and gave the answer {}".format(worker_id, assignmentid, worker_answer))
+	  				#return worker_answer
+		return dict(collections.Counter(ins_result)),dict(collections.Counter(attribute_result))
+		 
 
-# HIT attribtues
-# HITStatus, HITTypeId, Keywords, MaxAssignments, NumberofAssignmentsCompleted, Title, Reward Description
+	# retrive all reviewable HITs, return is a list of HITId
+	def _Search_HIT(self):
+		mtc = self.mtc
+		hits = mtc.get_all_hits()
+		hits_id = []
+		for hit in hits :
+			hits_id.append(hit.HITId)
+		return hits_id
 
-# get all HITs by filter keywords, our subjective KB HIT keywords could filter by 'knowledge'
-# return a list of HITId
-def _Get_Hits_Keyword (mtc, filter):
-	hits_list = mtc.get_all_hits()
-	answer = []
-	for hit in hits_list:
-		#cur_answer = _Retrive_HIT_Answer(mtc, hit_id)
-		#answer_pair.setdefault(hit_id,cur_answer)
-		if hasattr(hit,'Keywords') and hit.HITStatus != "Expired":
-			key_word = hit.Keywords
-			if key_word.find(filter) == -1:
-				continue
-			answer.append(hit.HITId)
-			#print("cur hit:{0}, Description:{2}, status:{1}".format(hit.HITId, hit.HITStatus, hit.Keywords))
-	return answer
+	# HIT attribtues
+	# HITStatus, HITTypeId, Keywords, MaxAssignments, NumberofAssignmentsCompleted, Title, Reward Description
 
-# Approve an assignment 
-# mtc.approve_assignment(assignment_id)
-# mtc.dispose_hit(hit_id)
-
-
-
+	# get all HITs by filter keywords, our subjective KB HIT keywords could filter by 'knowledge'
+	# return a list of HITId
+	def _Get_Hits_Keyword (self, filter):
+		mtc = self.mtc
+		hits_list = mtc.get_all_hits()
+		answer = []
+		for hit in hits_list:
+			if hasattr(hit,'Keywords') and hit.HITStatus != "Expired":
+				key_word = hit.Keywords
+				if key_word.find(filter) == -1:
+					continue
+				answer.append(hit.HITId)
+		return answer
 
 
-ACCESS_ID = 'AKIAJUT7UAPF2JZYLK6A'
-SECRET_KEY = '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'
-sandbox_HOST = 'mechanicalturk.sandbox.amazonaws.com'
-real_HOST = 'mechanicalturk.amazonaws.com'
+def read_data():
+	selected_list = ['id','name','country']
+	df = pd.read_csv('city.csv')[selected_list]
+	print df.info()
+	return df
 
-mtc = MTurkConnection(aws_access_key_id = ACCESS_ID, aws_secret_access_key = SECRET_KEY, host = sandbox_HOST)
 
-#_GET_Balance(mtc)
-#_Create_HIT(mtc)
-#answer_pair = _Retrive_Answers(mtc)
-#for hit_id in answer_pair:
-#	print("HIT:{0}, answer is {1}".format(hit_id, answer_pair[hit_id]))
-#_Retrive_HIT_Answer(mtc,"3R5LWXWHR09AANE1160M46QDXFVXGA")
-#_Retrive_ReviewableHits(mtc)
-#mtc.dispose_hit
 
-#hitList = _Get_Hits_Keyword(mtc,"research")
-#hit_answer = _Retrive_Answers(mtc, hitList)
-#for key in hit_answer:
-#	print("ID:{0}, Answer:{1}".format(key,hit_answer[key]))
 
-#print(_Retrive_HIT_Answer(mtc,"3R5LWXWHR09AANE1160M46QDXFVXGA"))
+# CREATE_HIT_FLAG = True
+if __name__ == '__main__':
+	sandboxFlag = True
+	CREATE_HIT_FLAG = False
+	NUM_OF_INIT_HITS = 4
+	START_INDEX = 0
 
-#test method create a Hit of bit city
-instList = ['New York','Beijing','London','Shanghai','Paris']
-contrylist = ['1','2','3','4','5']
-id1 = _Create_City_Hit(mtc,instList,contrylist,'big')
-for i in range(0,len(instList),5):
-	print i
-# id2 = _Create_City_Hit(mtc,instList[1],'big')
-# print id1
-[ins_result, attribute_result] = _Retrive_HIT_Answer(mtc,'3V7ICJJAZAHNL4IG19636K2IO8GB4C')
+	#test method create a Hit of bit city
+	df = read_data()
+	label = [None]* len(df['name'])
+	city2id = dict(zip(df['name'], range(len(df['name']))))
+	cityamt = Amt(sandboxFlag)
+	if CREATE_HIT_FLAG:
+		# for hit in mtc.get_all_hits():
+		# 	mtc.disable_hit(hit.HITId)
+		if sandboxFlag:
+			f = open('sand_hit_ids','a')
+		else:
+			f = open('hit_ids','a')
+		hit_ids = []
+		for i in range(START_INDEX,START_INDEX+NUM_OF_INIT_HITS*5,5):
+			namel =  list(df['name'][i:i+5])
+			countryl =  list(df['country'][i:i+5])
+			hit_id = cityamt._Create_City_Hit(namel,countryl,'big')
+			hit_ids.append(hit_id['hit_id'])
+			f.write(hit_id['hit_id']+'\n')  # python will convert \n to os.linesep
+		f.close()
+	else:
+		if sandboxFlag:
+			f = open('sand_hit_ids','r')
+		else:
+			f = open('hit_ids','r')
+		hit_ids = []
+		for line in f:
+			hit_ids.append(line.strip())
 
-for yn in ins_result:
-	print("Answer:{0},Count:{1}".format(yn,ins_result[yn]))
-for ar in attribute_result:
-	print("Attribtue:{0},Count:{1}".format(ar,attribute_result[ar]))
+	print hit_ids
+
+
+	# get answer
+	for i in range(START_INDEX,START_INDEX+NUM_OF_INIT_HITS*5,1):
+		label[i] = 0
+	for hit_id in hit_ids:
+		[ins_result, attribute_result] = cityamt._Retrive_HIT_Answer(hit_id)
+		for yn in ins_result:
+			if ins_result[yn]>0:
+				label[city2id[yn]] = 1
+			else:
+				label[city2id[yn]] = 0
+			print city2id[yn],label[city2id[yn]]
+			print("Answer:{0},Count:{1}".format(yn,ins_result[yn]))
+		for ar in attribute_result:
+			print("Attribtue:{0},Count:{1}".format(ar,attribute_result[ar]))
+	print label[START_INDEX:START_INDEX+NUM_OF_INIT_HITS*5]
+	print label
+	with open('label','w') as f:
+	    pickle.dump(label,f)
+	with open('label','rb') as f:
+	    label1 = pickle.load(f)
+	# print label1
--- classifer.py
+++ classifer.py
@@ -1,13 +1,23 @@
 import numpy as np
 import pandas as pd
+import pickle
 import sklearn
 from sklearn.preprocessing import StandardScaler
+from libact.base.dataset import Dataset, import_libsvm_sparse
+from libact.models import *
+from libact.query_strategies import *
+from libact.labelers import IdealLabeler,InteractiveLabeler
+from libact.base.interfaces import QueryStrategy,ContinuousModel,ProbabilisticModel
+from libact.utils import inherit_docstring_from, zip
+from amt import Amt
 
-def read_data(featurefile,datafile):
+def pre_data(featurefile,datafile):
+	selected_list = ['id','country','areaLand','foundingDate','areaMetro','areaTotal',\
+	'areaWater','percentageOfAreaWater','populationDensity','populationTotal','populationUrban','utcOffset','elevation']
 	k=0;
 	feature_list = list()
 	feature_dic = dict()
-	for line in open(featurefile, "r"):
+	for line in open(featurefile, 'r'):
 		x = line.strip().split(';')
 		xid = x[0].split(':')[1]
 		feature_name = x[1].split('/')[1]
@@ -17,12 +27,18 @@ def read_data(featurefile,datafile):
 	print feature_dic,feature_list
 	index = []
 	data = []
-	for line in open(datafile,"r"):
+	for line in open(datafile,'r'):
 		x = line.strip().split('\t')
-		entity = x[0]
-		index.append(entity)
-		features = x[1].split(';')
+		loc = x[0].index(',')
+		eid = x[0][:loc]
+		name = x[0][loc+1:]
+		if name.find('%')>0:
+			continue
+		index.append(eid)
+		features = x[1].split('$')
 		fdata = dict()
+		fdata['id'] = int(eid)
+		fdata['name'] = name
 		for f in features:
 			fid,fval = f.split(',',1)[:2]
 			if fval[0]!='"':
@@ -35,39 +51,128 @@ def read_data(featurefile,datafile):
 				if fid == '25480':
 					if len(fval)< 3 or len(fval)>7:
 						fval = 0.0
-			fdata[feature_list[feature_dic[fid]]] = fval;
+					else:
+						fval = int(fval)
+			fdata[feature_list[feature_dic[fid]]] = fval
 		data.append(fdata)
 	df = pd.DataFrame(data,index = index)
 
 	df.fillna(np.nan)
-	# noneed = ['areaCode','timeZone','postalCode','motto','leaderName','leaderParty','area']
-	selected_list = ['country','areaLand','foundingDate',\
-	'areaMetro','areaTotal','areaWater','percentageOfAreaWater','populationDensity','populationTotal','populationUrban','utcOffset','elevation']
+	# noneed = ['country','areaCode','timeZone','postalCode','motto','leaderName','leaderParty','area']
+	selected_list = ['id','name','country','areaLand','foundingDate','areaMetro','areaTotal',\
+	'areaWater','percentageOfAreaWater','populationDensity','populationTotal','populationUrban','utcOffset','elevation']
 	# print df['foundingDate'].astype('timedelta64[M]')
 
-	print df.info()
 	df['areaTotal'] = df['areaTotal'].fillna(df['areaWater']+df['areaLand'])
 	df['areaTotal'] = df['areaTotal'].fillna(df['area'])
+	areaW = df[df['areaTotal'] < df['areaWater']+df['areaLand']]['areaWater']
+	areaL = df[df['areaTotal'] < df['areaWater']+df['areaLand']]['areaLand']
+	areaIndex = df['areaTotal'] < df['areaWater']+df['areaLand']
+	print areaIndex
+	df.loc[areaIndex,'areaTotal'] = areaW + areaL
+	print df[df['areaTotal'] < df['areaWater']+df['areaLand']][['areaTotal','areaWater','areaLand']]
+
 	df['areaWater'] = df['areaWater'].fillna(df['areaTotal']-df['areaLand'])
 	df['areaWater'] = df['areaWater'].fillna(0)
 	df['areaMetro'] = df['areaMetro'].fillna(0)
 	df['elevation'] = df['elevation'].fillna(int(df['elevation'].mean()))
+	df['utcOffset'] = df['utcOffset'].fillna(int(df['utcOffset'].mean()))
 	df['foundingDate'] = df['foundingDate'].fillna(int(df['foundingDate'].mean()))
 	df['areaLand'] = df['areaLand'].fillna(df['areaTotal']-df['areaWater'])
 	df['populationUrban'] = df['populationUrban'].fillna(df['populationTotal'])
 	df['populationTotal'] = df['populationTotal'].fillna(df['populationUrban'])
-	df['populationDensity'] = df['populationDensity'].fillna(df['populationTotal']/df['areaTotal'])
-	
-	df['percentageOfAreaWater'] = df['percentageOfAreaWater'].fillna(df['areaWater']/df['areaTotal'])
+	df['populationDensity'] = df['populationDensity'].fillna(df['populationTotal']/df['areaTotal']*1000000)
+	df['percentageOfAreaWater'] = df['percentageOfAreaWater'].fillna(df['areaWater']/df['areaTotal']*100)
+	df['country'] = df['country'].str.strip('"')
 	print df[selected_list].info()
-	df[selected_list].dropna(subset = ['areaTotal']).to_csv('city.csv')
-	df = pd.get_dummies(df[selected_list],dummy_na = True).dropna()
-	d1 = pd.read_csv('city.csv')
-	print d1.info()
-	# X = df.values
-	# stdsc = StandardScaler()
-	# X_std = stdsc.fit_transform(X)
+	df[selected_list].dropna(subset = ['areaTotal','country','percentageOfAreaWater','populationTotal']).to_csv('city.csv')
+
+def read_data():
+	selected_atrributes = ['areaLand','foundingDate','areaMetro','areaTotal',\
+	'areaWater','percentageOfAreaWater','populationDensity','populationTotal','populationUrban','utcOffset','elevation']
+	selected_info = ['id','name','country']
+	df = pd.read_csv('city.csv')
+	return df[selected_atrributes],df[selected_info]
+
+def make_query(dataset,model,method):
+	model.train(dataset)
+	unlabeled_entry_ids, X_pool = zip(*dataset.get_unlabeled_entries())
+    
+	if isinstance(model, ContinuousModel):
+		dvalue = model.predict_proba(X_pool)
+	elif isinstance(model, ProbabilisticModel):
+		dvalue = model.predict_real(X_pool)
+
+	if method == 'lc':  # least confident
+		# ask_id = np.argmin(np.max(dvalue, axis=1))
+		ask_ids = np.argsort(np.max(dvalue, axis=1))[:5]
+
+	elif method == 'sm':  # smallest margin
+		if np.shape(dvalue)[1] > 2:
+	    # Find 2 largest decision values
+			dvalue = -(np.partition(-dvalue, 2, axis=1)[:, :2])
+		margin = np.abs(dvalue[:, 0] - dvalue[:, 1])
+		# ask_id = np.argmin(margin)
+		ask_ids = np.argsort(margin)[:5]
+
+	elif method == 'entropy':
+		entropy = np.sum(-dvalue * np.log(dvalue), axis=1)
+		# ask_id = np.argmax(entropy)
+		ask_ids = np.argsort(entropy)[-5:]
+	return [unlabeled_entry_ids[ask_id] for ask_id in ask_ids]
+
+def ask_hit(df,ask_ids,train_ds):
+	city2id = dict(zip(df['name'], range(len(df['name']))))
+	sandboxFlag = True
+	cityamt = Amt(sandboxFlag)
+	
+	if sandboxFlag:
+		f = open('sand_hit_ids','a')
+	else:
+		f = open('hit_ids','a')
+	hit_ids = []
+	namel =  [df['name'][i] for i in ask_ids]
+	countryl =  [df['country'][i] for i in ask_ids]
+	hit_id = cityamt._Create_City_Hit(namel,countryl,'big')
+	hit_ids.append(hit_id['hit_id'])
+	f.write(hit_id['hit_id']+'\n')  # python will convert \n to os.linesep
+	f.close()
+	finish_hits = set([x.HITId for x in cityamt.mtc.get_reviewable_hits()])
+	print finish_hits
+	# while not (hit_id['hit_id'] in finish_hits):
+	# 	finish_hits = set([x.HITId for x in cityamt.mtc.get_reviewable_hits()])
+	for hit_id in hit_ids:
+		[ins_result, attribute_result] = cityamt._Retrive_HIT_Answer(hit_id)
+		for yn in ins_result:
+			if ins_result[yn]>1:
+				train_ds.update(city2id[yn],1)
+			else:
+				train_ds.update(city2id[yn],0)
+			print("Answer:{0},Count:{1}".format(yn,ins_result[yn]))
+		for ar in attribute_result:
+			print("Attribtue:{0},Count:{1}".format(ar,attribute_result[ar]))
+	return train_ds
+
+def main():
+	df,amt_info_df = read_data()
+	X = StandardScaler().fit_transform(df.values)
+	with open('label','rb') as f:
+		label = pickle.load(f)
+	# y_train = [1,1,0,1,1,1,0,0,0,1,1,1,1,0,1]
+	print X.shape[0]
+	print len(label)
+	train_ds = Dataset(X,label)
+	model = LogisticRegression()
+	quota = 1
+	model.train(train_ds)
+	print model.score(train_ds)
+	for i in range(quota):
+		ask_ids = make_query(train_ds,model,'lc')
+		print ask_ids
+		train_ds = ask_hit(amt_info_df,ask_ids,train_ds)
+		model.train(train_ds)
+		print model.score(train_ds)
 
 
-
-read_data("testInstance_PropertyList.txt","testInstanceFeature.txt")
+#pre_data('testInstance_PropertyList.txt','testInstanceFeature1.txt')
+main()
